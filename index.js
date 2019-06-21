a89afc45a0d1c0dd5fb5228f1b084fb30b4e2b18
Betsi12 <betsi.salas12@gmail.com>
2019-06-21 16:08:04 -0400

    Instalación de dependencias y modificacion de package.json

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+const mdLinks = require("./src/md-links");
 module.exports = () => {
   // ...
 };
---
+++ src/md-links.js
@@ -0,0 +1,9 @@
+/*Librerias de node.js usadas*/
+const fs = require('fs');
+const nodepath = require('path');
+const marked = require('marked');
+const fetch = require('node-fetch');
+const fileHound = require('filehound');
+
+/*Función mdLinks conecta los archivos index y md-links*/
+
