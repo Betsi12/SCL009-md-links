eb176eaec082a964ea2b1987f57ad790e970546a
Betsi12 <betsi.salas12@gmail.com>
2019-06-22 20:22:16 -0400

    Primer test de la función md-links e instalación de la libreria marked en dependencias

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,14 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../md-links');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('Debería retornar 2 links para el archivo prueba.md', () => {    
+    expect(mdLinks.mdLinks('./prueba.md')).resolves.toEqual(['https://es.wikipedia.org/wiki/Markdown',
+    'https://nodejs.org/']);
+  });
+
+  it('Debería retornar error ENOENT, si se intenta leer un archivo que no existe (test2.md)', async()  => {
+    await expect(mdLinks.mdLinks('./test2.md')).rejects.toEqual("ENOENT");
   });
 
 });
