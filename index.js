b01dc32498bfadf7be7f30ae923d9683c44d1141
Betsi12 <betsi.salas12@gmail.com>
2019-06-24 09:19:45 -0400

    Se agrgan comentarios al codigo creado hasta ahora

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 const mdLinks = require("./md-links");
 
-mdLinks.mdLinks(process.argv[2],false).then((links)=>{
+mdLinks.extractLinksFile(process.argv[2])
+.then((links)=>{
   links.forEach(function (link) {
-    console.log(link);
-  });
-});
+    /*formato solicitado  de impresión texto maximo 50 carácteres*/
+    console.log(`${link.file} ${link.href} ${link.text.substring(0,50)}`);
+  }); 
+  
+})
+.catch(console.error);
--- md-links.js
+++ md-links.js
@@ -1,14 +1,39 @@
+/*Librerias de node.js usadas */
+const nodepath = require('path');
 const fs = require('fs')
-const markdownLinkExtractor = require('markdown-link-extractor');
+const marked = require('marked');
 
-const mdLinks = (path,options)=>{
+/*Función extractLinksFile  extrae los links de un archivo .md
+   Se crea una promesa con parametro resolve y reject para leer los archivos y entregar array de links
+   Se crea un array para guardar los links
+   Se guarda el contenido del archivo path como  string en la variable mardown
+   Se crea un renderer que en caso de la ejecución exitosa de la promesa, sustituirá el renderer por defecto, en vez de convertir
+   los links a html resolvera el arreglo de links.
+   Se guardan los links en el array */
+
+const extractLinksFile = (path)=>{
     return new Promise((resolve,reject)=>{
         try{
-            let markdown = fs.readFileSync(path).toString();
-            let links = markdownLinkExtractor(markdown);
+            if(nodepath.extname(path)!=".md"){ 
+                throw(new Error("Extensión no válida"));
+            }
+            let links=[]; 
+            let markdown = fs.readFileSync(path).toString();  
+            const renderer = new marked.Renderer(); 
+            renderer.link = function(href, title, text) { 
+                links.push({
+                    href: href,
+                    text: text,                  
+                    file:path
+                });
+            };
+            /*Se usa el renderer creado anteriormente*/
+            marked(markdown,{renderer:renderer});
+            /*Si la promesa se resuelve con exito el valor  de resolve es enviado a  mdLinks (.then)*/ 
             resolve(links);
         }
         catch(error){
+            /*Si la promesa falla, el valor reject se envia a  mdLinks (.catch)*/
             reject(error);
         }
         
@@ -16,5 +41,5 @@ const mdLinks = (path,options)=>{
 }
 
 module.exports={
-    mdLinks
+    extractLinksFile
 }
