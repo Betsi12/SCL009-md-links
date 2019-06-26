34fc35723463e390c714146d4c49e12f3fb440df
Betsi12 <betsi.salas12@gmail.com>
2019-06-26 14:51:04 -0400

    Agregando test de funcion validate link y stats

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,10 +2,11 @@ const mdLinks = require("../src/md-links");
 
 describe('mdLinks', () => {
 
-  it('Debería retornar 2 links para el archivo prueba.md', async() => {    
+  it('Debería retornar 3 links para el archivo prueba.md', async() => {    
     await expect(mdLinks.mdLinks('./prueba.md')).resolves.toEqual(
       [{href:'https://es.wikipedia.org/wiki/Markdown', text:'Markdown', file:'./prueba.md' },
-      {href: 'https://nodejs.org/', text:'Node.js', file:'./prueba.md'}]);
+      {href: 'https://nodejs.org/', text:'md-links', file:'./prueba.md'},
+      {href: 'https://user-images.githubusercontent.com',text:'Node.js', file:'./prueba.md'}]);
   });
   
   it('Debería retornar error para el archivo prueba2.md', async()  => {
@@ -19,14 +20,20 @@ describe('mdLinks', () => {
   it('Debería retornar 2 links para el archivo prueba.md validando su status y statusText', async () => {
     await expect(mdLinks.mdLinks('./prueba.md',{validate:true})).resolves.toEqual(
     [{href:'https://es.wikipedia.org/wiki/Markdown', text:'Markdown', file:'./prueba.md',status:200,statusText:'OK' },
-    {href: 'https://nodejs.org/', text:'Node.js', file:'./prueba.md',status:200,statusText:'OK'}]);
+    {href: 'https://nodejs.org/', text:'Node.js', file:'./prueba.md', status:200, statusText:'OK'}]);
   });
 
   it('Debería retornar un error al chequear un link no válido', async()=>{
     await expect(mdLinks.validateLink([{href:'https://holi.wikipedia.org/wiki/Markdown', 
     text:'Markdown', file:'./prueba.md'}])).resolves.toEqual([{href:'https://holi.wikipedia.org/wiki/Markdown', 
-    text:'Markdown', file:'./prueba.md',status:0, 
-    statusText:"ENOTFOUND" }]);
+    text:'Markdown', file:'./prueba.md', status:0, statusText:"ENOTFOUND" }]);
+  });
+
+  it('Debería retornar la estadística linksTotal:3 y linksUnique:3 para el archivo prueba.md',()=>{
+    expect(mdLinks.statsLinks(
+    [{href:'https://es.wikipedia.org/wiki/Markdown'},{href: 'https://nodejs.org/'},
+    {href: 'https://user-images.githubusercontent.com'}])).toEqual({"linksTotal": 3, "linksUnique": 3});
   });
   
 });
+
