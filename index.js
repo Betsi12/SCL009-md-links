eb5c43ba92d6b4b8f8c7727feee1f61a5672c09c
Betsi12 <betsi.salas12@gmail.com>
2019-06-29 20:17:43 -0400

    Agregando colores a las respuestas en la terminal

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 #!/usr/bin/env node
 
 const mdLinks = require("./src/md-links");
+const chalk = require('chalk');
 
 if((process.argv[3]==="--validate" && process.argv[4]==="--stats") || 
-    (process.argv[3]==="--stats" && process.argv[4]==="--validate")){
-      mdLinks.mdLinks(process.argv[2], {validate: true})
-     .then((links)=>{
+  (process.argv[3]==="--stats" && process.argv[4]==="--validate")){
+  mdLinks.mdLinks(process.argv[2], {validate: true})
+    .then((links)=>{
+      links=links.flat();
       let responseStats=mdLinks.statsLinks(links, {validate: true});
-      console.log(`Total: ${responseStats.linksTotal}`);
-      console.log(`Unique: ${responseStats.linksUnique}`);
-      console.log(`Broken: ${responseStats.linksBroken}`)
+      console.log(chalk.bold.green(`Total: ${responseStats.linksTotal}`));
+      console.log(chalk.bold.yellow(`Unique: ${responseStats.linksUnique}`));
+      console.log(chalk.bold.red(`Broken: ${responseStats.linksBroken}`));
+      console.log(chalk.bold.black.bgWhiteBright(`HTTP Response Status Codes`));
+      console.log(chalk.bold.green(`- Information responses: ${responseStats.informationResponses}`));
+      console.log(chalk.bold.green(`- Successful responses: ${responseStats.successfulResponses}`));
+      console.log(chalk.bold.yellow(`- Redirection messages: ${responseStats.redirectionMessages}`));
+      console.log(chalk.bold.red(`- Client error responses: ${responseStats.clientErrorResponses}`));
+      console.log(chalk.bold.red(`- Server error responses: ${responseStats.serverErrorResponses}`));
     })
     .catch(console.error);
   }
-else if(process.argv[3]==="--validate"){
-  mdLinks.mdLinks(process.argv[2],{validate:true})
-    .then((links) => {
-      links=links.flat();
-      links.forEach(function (link) {   
-        console.log(`${link.file} ${link.href} ${link.status} ${link.statusText} ${link.text.substring(0,50)}`);
-      }); 
-    })
-    .catch(console.error);
-}else if(process.argv[3]==="--stats"){
-  mdLinks.mdLinks(process.argv[2])
-    .then((links) => {
-      links=links.flat();
-      let responseStats=mdLinks.statsLinks(links);
-      console.log(`Total: ${responseStats.linksTotal}`);
-      console.log(`Unique: ${responseStats.linksUnique}`);
-    })
-    .catch(console.error);
-
-}else {
-  mdLinks.mdLinks(process.argv[2])
-    .then((links) => {
-      links=links.flat();
-      links.forEach(function (link) {   
-        console.log(`${link.file} ${link.href} ${link.text.substring(0,50)}`);
-      }); 
-    })
-    .catch(console.error);
-}
-
+  else if(process.argv[3]==="--validate"){
+    mdLinks.mdLinks(process.argv[2],{validate:true})
+      .then((links) => {
+        links=links.flat();
+        links.forEach(function (link) {   
+          console.log(`${chalk.bold.bgBlue(link.file)} ${chalk.bold.blue(link.href)} ${chalk.bold.rgb(255, 167, 38)(link.status)} ${chalk.yellow(link.statusText)} ${chalk.bold.cyan(link.text.substring(0,50))}`);
+        }); 
+      })
+      .catch(console.error);
+  }else if(process.argv[3]==="--stats"){
+    mdLinks.mdLinks(process.argv[2])
+      .then((links) => {
+        links=links.flat();
+        let responseStats=mdLinks.statsLinks(links);
+        console.log(chalk.bold.green(`Total: ${responseStats.linksTotal}`));
+        console.log(chalk.bold.yellow(`Unique: ${responseStats.linksUnique}`));
+      })
+      .catch(console.error);
+  
+  }else {
+    mdLinks.mdLinks(process.argv[2])
+      .then((links) => {
+        links=links.flat();
+        links.forEach(function (link) {   
+          console.log(`${chalk.bold.bgBlue(link.file)} ${chalk.bold.blue(link.href)} ${chalk.bold.cyan(link.text.substring(0,50))}`);
+        }); 
+      })
+      .catch(console.error);
+  }
+  
+  
+  
